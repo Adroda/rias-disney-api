d54095e6c6c0f0f72b581f8c40591939618c657b
adroda <adrianchi_91@hotmail.com>
2023-05-18 18:13:17 -0300

    random service working, fetching character by name brings more than 1 character and might bring trouble

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,36 @@
 import SearchBar from './SearchBar';
 import RandomButton from './RandomButton';
 import '../../styles/header/header.scss';
+import {
+  fetchCharacter,
+  fetchRandomCharacter,
+} from '../../services/DisneyServices';
 
 const Header = ({ card, setCard }) => {
+  let logger = async () => {
+    let randomCharacter = await fetchRandomCharacter();
+    /* let character = await fetchCharacter('Mickey Mouse');
+    console.log(
+      '🚀 ~ file: header.jsx:14 ~ logger ~ character:',
+      character.data
+    );*/
+  };
+  logger();
 
   return (
     <div className="header">
       <div className="logo">
-      <section className="logo-wrapper">
-        <div className="logo-top">RITA TEAM</div>
-        <div className="logo-bottom">RITA TEAM</div>
-      </section>
+        <section className="logo-wrapper">
+          <div className="logo-top">RITA TEAM</div>
+          <div className="logo-bottom">RITA TEAM</div>
+        </section>
       </div>
       <div className="titulo">
-      <p className='title'>DISNEY CHARACTERS </p> 
+        <p className="title">DISNEY CHARACTERS </p>
       </div>
       <div className="cont">
-      <SearchBar />
-      <RandomButton />
+        <SearchBar />
+        <RandomButton />
       </div>
     </div>
   );
--- src/services/DisneyServices.jsx
+++ src/services/DisneyServices.jsx
@@ -1,13 +1,26 @@
 export const BASE_URL = 'https://api.disneyapi.dev/character';
 export const API_KEY = process.env.REACT_APP_APIKEY;
+const totalCharacters = 7438;
 
-const fetchCharacter = (id,state) => {
-  const URL = `${BASE_URL}/${id}`;
-  
-  return fetch(URL)
-    .then((response) => response.json())
-    .then((character) => character);
+let fetchCharacter = async (name) => {
+  const URL = `${BASE_URL}?name=${name}`;
+  const response = await fetch(URL);
+  let data = await response.json();
+
+  return data;
 };
 
-export { fetchCharacter };
- 
+let fetchRandomCharacter = async () => {
+  let count = 0;
+  let data;
+  while (count === 0) {
+    let id = Math.floor(Math.random() * totalCharacters);
+    const URL = `${BASE_URL}/${id}`;
+    const response = await fetch(`${BASE_URL}/${id}`);
+    data = await response.json();
+    count = data.info.count;
+  }
+  return data;
+};
+
+export { fetchCharacter, fetchRandomCharacter };
